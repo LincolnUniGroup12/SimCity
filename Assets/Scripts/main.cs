be6b272891b591652b4886555416165d4aeb78e7
dannyhodge <dannyhodge95@gmail.com>
2017-03-06 00:17:34 +0000

    Camera Control, folder layout and demo scene
    I've added a folder layout for us to use that makes everything a bit
    easier. Basically, Materials is there for the unity materials (they get
    created automatically if you drag a texture onto a gameobject), Models
    is there for any 3d models, Prefabs is for any gameobjects that you need
    storing (e.g. for instantiation), Scenes is for any scene files (we will
    likely just need a menu scene and a game scene), Scripts is for all the
    scripts, we may need sub folders if there are a lot of them, and
    Textures is for the image files that are used to create a Material (so
    .png, .jpg, .psd files)..
    
    I've also created a cameraControl script that is attached to the Camera
    Movement gameobject, and thrown together a demo scene to play around
    with the camera in (it's hard to test when you can't see depth). The
    script is documented if you don't quite understand what's going on.

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,9 @@
+﻿/* Ruoyu Gu*/
+using UnityEngine;
+using System.Collections;
+public class main : MonoBehaviour 
+{
+	public static int money=0,population=0,day=0,Map_W = 100,Map_L = 100;
+	public static float Block_Size = 0.9f;	//	格子大小
+
+}
