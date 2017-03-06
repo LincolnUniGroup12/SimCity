d184b6d626d7b4853e4ddc343b6bb86f9d7399fc
dannyhodge <dannyhodge95@gmail.com>
2017-03-06 13:17:00 +0000

    Budget features
    Added a gui that changes tax level and some basic expenditures.
    Added cityStats script that calculates the overall profitability of the
    city. Needs a lot of fine tuning and documentation. Expanded the demo
    scene, but if it lags too hard just delete most of the houses.

---
+++ Assets/Scripts/cityStats.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cityStats : MonoBehaviour {
+
+
+	public float population = 1000;
+	public float availableJobs = 700;
+	public int averageIncome = 20000;
+
+	public float taxRate = 0;
+	public float healthcareSpending = 0;
+	public float educationSpending = 0;
+
+	public GameObject taxSlider;
+	public GameObject taxlevel;
+	public GameObject healthSlider;
+	public GameObject healthlevel;
+	public GameObject educationSlider;
+	public GameObject educationlevel;
+
+	public GameObject overallProfit;
+	public float profit = 0;
+	public float percEmployed;
+	public float workingPopulation;
+	public float overallIncome;
+	public float overallIncomeTax;
+	public float overallExpenses;
+
+
+
+
+	void Update() {
+		percEmployed = availableJobs / population;
+		workingPopulation = population * percEmployed;
+		overallIncome = workingPopulation * averageIncome;
+		overallIncomeTax = overallIncome * (taxRate / 100);
+		overallExpenses = healthcareSpending + educationSpending;
+		profit = overallIncomeTax - overallExpenses;
+
+		overallProfit.GetComponent<Text> ().text = "£" + profit;
+	}
+
+
+	public void ChangeTaxRate() {
+		taxRate = taxSlider.GetComponent<Slider>().value;
+		taxlevel.GetComponent<Text> ().text = taxRate + "%";
+	}
+
+
+	public void ChangeHealthRate() {
+		healthcareSpending = healthSlider.GetComponent<Slider>().value;
+		healthlevel.GetComponent<Text> ().text =  "£" + healthcareSpending;
+	}
+
+	public void ChangeEducationRate() {
+		educationSpending = educationSlider.GetComponent<Slider>().value;
+		educationlevel.GetComponent<Text> ().text = "£" + educationSpending;
+	}
+}
