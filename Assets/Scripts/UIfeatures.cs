bebbf8ffb3d94ba0e288c284d8d07f10fd1a559f
dannyhodge <dannyhodge95@gmail.com>
2017-03-08 12:37:55 +0000

    Finished Calendar
    Finilaized calendar

--- Assets/Scripts/UIfeatures.cs
+++ Assets/Scripts/UIfeatures.cs
@@ -35,20 +35,30 @@ public class UIfeatures : MonoBehaviour {
 		if (month == 2) {
 			if (day == 28) {
 				month++;
-				day = 0;
+				day = 1;
+				dayFloat = 1f;
 			}
 		}
-		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 | month == 12) {
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) {
 			if (day == 31) {
 				month++;
-				day = 0;
+				dayFloat = 1f;
+				day = 1;
 			}
 		}
 		if (month == 4 || month == 6 || month == 9 || month == 11) {
 			if (day == 30) {
 				month++;
-				day = 0;
+				day = 1;
+				dayFloat = 1f;
 			}
+		}
+
+		if (month == 12 && day == 32) {
+			year++;
+			month = 1;
+			day = 1;
+			dayFloat = 1;
 		}
 	}
 
@@ -68,6 +78,11 @@ public class UIfeatures : MonoBehaviour {
 	}
 
 	public void ff1() {
-
+		Time.timeScale = 0.66f;
 	}
+
+	public void ff2() {
+		Time.timeScale = 15f;
+	}
+
 }
