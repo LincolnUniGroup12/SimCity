7326e595f12291ca7d697b300cda82060b0da7aa
SilverDragon22 <alexweber20@hotmail.co.uk>
2017-03-15 15:54:30 +0000

    Alex's Tilemap progress

---
+++ Assets/Scripts/TileMapInspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapInspector : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+
+        if (GUILayout.Button("Regenerate"))
+        {
+            TileMap tileMap = (TileMap)target;
+            tileMap.BuildMesh();
+        } 
+    }
+}
--- Assets/Scripts/tileMap.cs
+++ Assets/Scripts/tileMap.cs
@@ -2,58 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tileMap : MonoBehaviour {
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+
+public class TileMap : MonoBehaviour {
+    //number of tiles to create and their size
+    public int size_x = 100;
+    public int size_z = 50;
+    public float tileSize = 1.0f;
 
 	// Use this for initialization
 	void Start () {
-		BuildMesh ();
-	}
-	
-
-	void BuildMesh() {
-		Vector3[] vertices = new Vector3[4];
-		int[] triangles = new int[2 * 3];
-		Vector3[] normals = new Vector3[4];
-		Vector2[] uv = new Vector2[4];
-
-		vertices [0] = new Vector3 (0, 0, 0);
-		vertices [1] = new Vector3 (100, 0, 0);
-		vertices [2] = new Vector3 (0, 0, -100);
-		vertices [3] = new Vector3 (100, 0, -100);
-
-		triangles [0] = 0;
-		triangles [1] = 3;
-		triangles [2] = 2;
-
-		triangles [3] = 0;
-		triangles [4] = 1;
-		triangles [5] = 3;
-
-		normals [0] = Vector3.up;
-		normals [1] = Vector3.up;
-		normals [2] = Vector3.up;
-		normals [3] = Vector3.up;
-
-		uv [0] = new Vector2 (0,1);
-		uv [1] = new Vector2 (1,1);
-		uv [2] = new Vector2 (0,0);
-		uv [3] = new Vector2 (1,0);
-
-		Mesh mesh = new Mesh ();
-
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-		mesh.normals = normals;
-		mesh.uv = uv;
-
-		MeshFilter meshfilter = GetComponent<MeshFilter> ();
-		MeshRenderer meshrenderer = GetComponent<MeshRenderer> ();
-		MeshCollider meshcollider = GetComponent<MeshCollider> ();
-
-		meshfilter.mesh = mesh;
-
-
+        BuildMesh();
 	}
 
+    public void BuildMesh()
+    {
+        int numTiles = size_x * size_z;
+        int numTriangles = numTiles * 2;
+        int vsize_x = size_x + 1;
+        int vsize_z = size_z + 1;
+        int numVerts = vsize_x * vsize_z;
 
+        //generate mesh data
+        Vector3[] verticies = new Vector3[numVerts];
+        Vector3[] normals = new Vector3[numVerts];
+        Vector2[] uv = new Vector2[numVerts];
+
+        int[] triangles = new int[numTriangles * 3];
+
+        int x, z;
+
+        for ( z = 0; z < vsize_z; z++)
+        {
+            for ( x = 0; x < vsize_x; x++)
+            {
+                verticies[z * vsize_x + x] = new Vector3(x*tileSize,0,z*tileSize);
+                normals[z * vsize_x + x] = Vector3.up;
+                uv[z * vsize_x + x] = new Vector2((float) x / vsize_x, (float)z / vsize_z);
+
+            }
+        }
+
+        for (z = 0; z < size_z; z++)
+        {
+            for (x = 0; x < size_x; x++)
+            {
+                //Debug.Log("triangle" + x + "," + z);
+                int squareIndex = z * size_x + x;
+                int triOffset = squareIndex * 6;
+
+                triangles[triOffset + 0] = z * vsize_x + x + 0;
+                triangles[triOffset + 1] = z * vsize_x + x + vsize_x+ 0;
+                triangles[triOffset + 2] = z * vsize_x + x + vsize_x + 1;
+
+                triangles[triOffset + 3] = z * vsize_x + x + 0;
+                triangles[triOffset + 4] = z * vsize_x + x + vsize_x + 1;
+                triangles[triOffset + 5] = z * vsize_x + x + 1;
+
+
+            }
+        }
+
+
+
+                //create a new mesh and populate with the data
+                Mesh mesh = new Mesh();
+        mesh.vertices = verticies;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        //assign mesh to game object
+        MeshFilter mesh_filter = GetComponent<MeshFilter>();
+        MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
+        MeshCollider mesh_collider = GetComponent<MeshCollider>();
+
+        mesh_filter.mesh = mesh;
+        mesh_collider.sharedMesh = mesh;
+
+
+    }
 }
