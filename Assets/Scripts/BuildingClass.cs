8a60d61304cea0204e29222d49e453e90e71cb6e
grykolk <grykolk@outlook.com>
2017-03-18 22:32:48 +0000

    changes

--- Assets/Scripts/BuildingClass.cs
+++ Assets/Scripts/BuildingClass.cs
@@ -3,21 +3,19 @@ using UnityEngine;
 using System.Collections;
 public class BuildingClass : MonoBehaviour
 {
-    public GameObject factoryPre;
-    public GameObject apartmentPre;
-    public bool is_building = false;
     public int wide, leigh; // building size
     int price;  //building price
-    public building_types type;
     bool canBuild = true;
+    public building_types type;
+    public build_state state;
     void State_Machine()
     {
-        switch (is_building)
+        switch (state)
         {
-            case false:
+            case build_state.Normal:
                 normal();
                 break;
-            case true:
+            case build_state.Build:
                 build();
                 break;
         }
@@ -38,57 +36,23 @@ public class BuildingClass : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(0))
         {
-            is_building = false;
+            state = build_state.Normal;
+            main.build_mode = false;
         }
     }
     void Update()
     {
         State_Machine();
     }
-
-
-    public enum building_types
-    {
-        factory,
-        apartment,
-    }
-    public void DidClickBuildFactory()
-    {
-        start_build(building_types.factory);
-    }
-
-
-    public void DidClickBuildApartment()
-    {
-        start_build(building_types.apartment);
-    }
-     public void start_build(building_types t)
-    {
-        //	判断是否处于正常状态
-        if (!main.build_mode)
-        {
-            //	创建游戏对象用的预设体
-            GameObject tempPre = null;
-            //	判断建筑物类型
-            switch (t)
-            {
-                case building_types.factory:
-                    //	建造兵营
-                    tempPre = factoryPre;
-                    break;
-                case building_types.apartment:
-                    //	建造旗帜
-                    tempPre = apartmentPre;
-                    break;
-
-            }
-            //	用预设体创建建筑物
-            GameObject obj = Instantiate(tempPre, new Vector3(1000f, 1000f, 1000f), Quaternion.identity) as GameObject;
-            //	设置建筑物为建造状态
-            obj.GetComponent<BuildingClass>().is_building = true;
-            //	设置当前状态为建造状态
-            main.build_mode = true;
-           
-        }
-    }
+    
+}
+public enum building_types
+{
+    factory,
+    apartment,
+}
+public enum build_state
+{
+    Normal=0,
+    Build,
 }
---
+++ Assets/Scripts/event_control.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class event_control : MonoBehaviour {
+    public GameObject factoryPre;
+    public GameObject apartmentPre;
+    public build_bottom[] button;
+    public void DidClickBuildFactory()
+    {
+        start_build(building_types.factory);
+    }
+
+
+    public void DidClickBuildButton()
+    {
+        start_build(building_types.apartment);
+    }
+    public void start_build(building_types t)
+    {
+        //	判断是否处于正常状态
+        if (!main.build_mode)
+        {
+            //	创建游戏对象用的预设体
+            GameObject tempPre = null;
+            //	判断建筑物类型
+            switch (t)
+            {
+                case building_types.factory:
+                    //	建造兵营
+                    tempPre = factoryPre;
+                    break;
+                case building_types.apartment:
+                    //	建造旗帜
+                    tempPre = apartmentPre;
+                    break;
+
+            }
+            //	用预设体创建建筑物
+            GameObject obj = Instantiate(tempPre, new Vector3(1000f, 1000f, 1000f), Quaternion.identity) as GameObject;
+            //	设置建筑物为建造状态
+            obj.GetComponent<BuildingClass>().state = build_state.Build;
+            //	设置当前状态为建造状态
+            main.build_mode = true;
+
+        }
+    }
+}
