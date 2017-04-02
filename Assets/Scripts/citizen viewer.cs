7c22848e8e2b48ab8cf0874df84750471fec846b
sean910358 <seansilvia30@gmail.com>
2017-04-03 00:46:49 +0100

    Add files via upload

---
+++ Assets/Scripts/citizen viewer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class citizenviewer : MonoBehaviour {
+    private int Police = 10;
+    private int Hospital = 10;
+    private int School = 10;
+    private float Average;
+
+
+    void Start () {
+        Average = Police + Hospital + School;
+        if (Average >= 30)
+        {
+            Debug.Log("happy");
+        }
+        else if (Average < 20 && Average > 10) {
+            Debug.Log("normal");
+        }
+        else
+        {
+            Debug.Log("upset");
+        }
+    }
+	
+	
+	void Update () {
+		
+	}
+}
