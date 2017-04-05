b19cfccbf50ba03b104310f3198ccb29008f71bc
SilverDragon22 <alexweber20@hotmail.co.uk>
2017-04-05 10:33:11 +0100

    Updated building placement

--- Assets/Scripts/TileMapMouse.cs
+++ Assets/Scripts/TileMapMouse.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 public class TileMapMouse : MonoBehaviour {
 
     tileMap _tileMap;
-    public GameObject Cube;
+    public GameObject House1;
+    public GameObject House2;
+    public GameObject House3;
+    bool select1;
+    bool select2;
+    bool select3;
 
     Vector3 currentTileCoord;
 
@@ -39,7 +44,45 @@ public class TileMapMouse : MonoBehaviour {
         {
             Debug.Log("Click!");
             Debug.Log("Tile: " + currentTileCoord.x + "," + currentTileCoord.z);
-            Instantiate(Cube, new Vector3(currentTileCoord.x + 0.5f, 0.0f, currentTileCoord.z + 0.5f), Quaternion.identity);
+            if (select1)
+            {
+                Instantiate(House1, new Vector3(currentTileCoord.x + 0.5f, 0.0f, currentTileCoord.z + 0.5f), Quaternion.identity);
+            }
+            else if (select2)
+            {
+                Instantiate(House2, new Vector3(currentTileCoord.x + 0.5f, 0.0f, currentTileCoord.z + 0.5f), Quaternion.identity);
+            }
+            else if (select3)
+
+            {
+                Instantiate(House3, new Vector3(currentTileCoord.x + 0.5f, 0.0f, currentTileCoord.z + 0.5f), Quaternion.identity);
+            }
+
+
         }
-	}
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            select1 = true;
+            select2 = false;
+            select3 = false;
+            Debug.Log("House 1 selected");
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            select1 = false;
+            select2 = true;
+            select3 = false;
+            Debug.Log("House 2 selected");
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            select1 = false;
+            select2 = false;
+            select3 = true;
+            Debug.Log("House 3 selected");
+
+        }
+    }
 }
